cf03dbbf761b3a4c73297c3b55f0219916b26192
PashaKoplyk <pashabroneboy@gmail.com>
2026-01-23 16:14:55 +0000

    modul 10

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,3 +1,9 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
 console.log('Tіmes');
 
 
